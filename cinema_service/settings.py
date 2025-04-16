18c7568d2aef72ad586a8f9fd21a43a5bbca0bbd
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-04-16 13:11:17 +0300

    Solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -49,10 +49,6 @@ INSTALLED_APPS = [
     "user",
 ]
 
-# REST_FRAMEWORK = {
-#     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-#     "PAGE_SIZE": 5
-# }
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
